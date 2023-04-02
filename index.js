47ee10a33ae232a07ce678543f1c7725317e0298
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-04-01 23:03:13 -0500

    feat: add isThereAWinner function and description

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const io = new Server({
   }
 });
 
+/**
+ * isThereAWinner returns true if there is a winner in the match
+ * other ways returns false.
+ *
+ * This receives memoizated data and counts every contained node
+ * if the count is greater than 2, it means there is a winner returning
+ * true, any ways it returns false.
+ * */
+const isThereAWinner = (data) => {
+	return 0
+}
+
 let id = "12345"
 
 io.on("connection", (socket) => {
