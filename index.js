dd2149fd1ccf9aea32c33ddac53b8e56d0347bf8
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-04-02 22:47:07 -0500

    feat: add tic-tac-toe game logic

--- index.js
+++ index.js
@@ -15,20 +15,43 @@ const io = new Server({
  * true, any ways it returns false.
  * */
 const checkForWinner = (data) => {
-	return 0
-  p
+  const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],    // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],    // Columns
+    [0, 4, 8], [2, 4, 6]                // Diagonals
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+      return gameState[a];
+    }
+  }
+  return null;
 }
 
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
 io.on("connection", (socket) => {
-  
-  socket.on("game", arg => {
-    console.log(arg); // world
-  });
+  console.log('New client connected');
 
-  socket.on("queue", arg => {
-    socket.emit("game", "here is your shit: " + arg)
-  });
+  // Send current game state to player who just connected
+  socket.emit('gameState', gameState);
 
+  // Listen for player move events
+  socket.on('move', (data) => {
+    // Update game state with player's move
+    gameState[data.index] = data.symbol;
+
+    // Broadcast updated game state to other player
+    socket.broadcast.emit('gameState', gameState);
+
+    // Check for winner
+    const winner = checkForWinner(gameState);
+    if (winner) {
+      io.emit('winner', winner);
+    }
+  });
 });
 
 io.listen(5000);
